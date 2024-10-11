478ffd7e8ec644ed705e2c14f2fb3e976f82f40a
Kris Hvattum <klhvattum@live.com>
2024-10-11 07:45:02 -0700

    redeploy

--- src/app/api/speech-to-text/route.js
+++ src/app/api/speech-to-text/route.js
@@ -66,7 +66,11 @@ export async function POST(req) {
     // const client = new SpeechClient();
 
     try {
-        const serviceAccount = new Buffer(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'base64').toString();
+        const base64ServiceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64;
+        const decodedJson = Buffer.from(base64ServiceAccount, 'base64').toString('utf-8');
+        console.log('Decoded JSON:', decodedJson); // Check what is actually getting decoded
+
+        const serviceAccount = JSON.parse(decodedJson);
 
         const client = new SpeechClient({
             credentials: serviceAccount,
