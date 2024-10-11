6006260369c7523f7dc2dfad24f082851b00902c
Kris Hvattum <klhvattum@live.com>
2024-10-11 13:58:31 -0700

    redeploy

--- src/app/api/speech-to-text/route.js
+++ src/app/api/speech-to-text/route.js
@@ -1,6 +1,6 @@
 import { SpeechClient } from '@google-cloud/speech';
 
-export async function handler(req, res) {
+export async function POST(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Only POST requests are allowed' });
     }
