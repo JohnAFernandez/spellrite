08c33c6b82eb86721c1e98886e6a0ae722c528b5
Kris Hvattum <klhvattum@live.com>
2024-10-10 23:52:09 -0700

    redeploy

--- src/app/api/speech-to-text/route.js
+++ src/app/api/speech-to-text/route.js
@@ -66,7 +66,7 @@ export async function POST(req) {
     // const client = new SpeechClient();
 
     try {
-        const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+        const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'base64').toString('ascii');
 
         const client = new SpeechClient({
             credentials: {
