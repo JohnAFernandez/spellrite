200af626d9a275549627cde8841708ca777f0d40
Kris Hvattum <klhvattum@live.com>
2024-10-13 19:31:21 -0700

    fix issue with refreshing

--- src/GlobalStateContext.js
+++ src/GlobalStateContext.js
@@ -1,10 +1,28 @@
 "use client"
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const GlobalStateContext = createContext();
 
 export const GlobalStateProvider = ({ children }) => {
-    const [test, setTest] = useState('');
+    const [test, setTestState] = useState(() => {
+        // Initialize the state from localStorage if available
+        if (typeof window !== "undefined") {
+            const storedTest = localStorage.getItem('test');
+            return storedTest ? JSON.parse(storedTest) : null;
+        }
+        return null;
+    });
+
+    // Sync the state with localStorage whenever it changes
+    useEffect(() => {
+        if (test) {
+            localStorage.setItem('test', JSON.stringify(test));
+        }
+    }, [test]);
+
+    const setTest = (newTest) => {
+        setTestState(newTest);
+    };
 
     return (
         <GlobalStateContext.Provider value={{ test, setTest }}>
